4d2df19f6d728a3e53d00fd5b6bd234401ea3559
Jongun Jeong <skeep194@knu.ac.kr>
2026-03-31 22:22:05 +0900

    chore: get_UID link docs, move error raise to event handler

--- src/pyerapi/client.py
+++ src/pyerapi/client.py
@@ -3,10 +3,18 @@ from .config import config
 
 class Client:
     def __init__(self, api_key: str):
+        def handle_http_error(response: httpx.Response):
+            # TODO: API 명세에 있는 상태 확인 후 커스텀 에러 객체 raise, message가 있는 경우 확인 가능하게
+            response.raise_for_status()
+            pass
+
         self._httpClient = httpx.Client(
             base_url= config.base_url,
             headers={
                 "x-api-key": api_key
+            },
+            event_hooks={
+                'response': [handle_http_error]
             }
         )
 
--- src/pyerapi/endpoints/users.py
+++ src/pyerapi/endpoints/users.py
@@ -6,9 +6,11 @@ class UsersAPI:
         self._client = client
 
     def get_UID(self, nickname: str):
+        """
+        https://developer.eternalreturn.io/static/media/OpenAPI_KR_20251118.html#get-user-by-nickname
+        """
         params = {"query": nickname}
         resp = self._client._httpClient.get(f'v1/user/nickname', params=params) \
-            .raise_for_status() \
             .json()
 
         return ApiResponse[UID](**resp).data
